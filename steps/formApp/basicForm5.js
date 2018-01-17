061eca7f6148a77231318e5ffcb153a473c779d6
Alex Lerner <lernera@redi.com>
2018-01-17 17:06:45 -0500

    started to break up my form into a separate input field component which communicate up to the form

--- steps/formApp/basicForm5.js
+++ steps/formApp/basicForm5.js
@@ -2,6 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import validator from 'validator';
 
+export default class FormDemo extends React.Component {
+    static propTypes = {
+        placeholder: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        value: PropTypes.string,
+        validate: PropTypes.func,
+        onChange: PropTypes.func.isRequired
+    };
+    state = {
+        value: this.props.value,
+        error: false
+    }
+
+    componentWillRecieveProps(update) {
+        this.setState({value: update.value});
+    }
+
+    /* from input to here, from here further up to the parent */
+    onChange = (evt) => {
+        const name = this.props.name;
+        const value = evt.target.value;
+        const error = this.props.validate ? this.props.validate(value) : false;
+        this.setState({value, error});
+        this.props.onChange({name, value, error});
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                    placeholder={this.props.placeholder}
+                    value={this.props.value}
+                    onChange={this.onChange}
+                />
+                <span style={{color:'red'}}>{this.state.error}</span>
+            </div>
+        );
+    }
+}
+
 // using ref gives direct access to the DOM element
 export default class FormDemo extends React.Component {
     state = {

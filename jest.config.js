de41e228b49b7ae7021aae9902b6e08a1a99271f
Alex Lerner <alernerdev@gmail.com>
2018-01-20 16:11:09 -0500

    configured jest to load config, got my first test with enzyme

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	verbose: true,
+	setupFiles: [
+		"./testsSetup/testsSetup.js"
+	]
+};
--- src/listapp/tests/List.test.js
+++ src/listapp/tests/List.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import List from '../js/List';
 
-it('ListApp renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<List />, div);
+/* expect works, but eslint is complaining */
+/* eslint-disable no-undef */
+
+describe('ListApp', () => {
+	it('should have the `th` "Items"', () => {
+		const wrapper = shallow(
+			<List />
+		);
+		expect(wrapper.contains(<th>Items</th>)).toBe(true);
+	});
 });
+
